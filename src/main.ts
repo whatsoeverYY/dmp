5f016bd24515a849589926797a156993b17dc6bb
Chen Yinying <chenyinying@patsnap.com>
2024-03-29 10:23:22 +0800

    add code highlight

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import './assets/main.css';
-
-import Vue from 'vue';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -9,6 +7,15 @@ import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+/*黑色主题*/
+// import 'highlight.js/styles/atom-one-dark.css';
+/*白色主题*/
+import 'highlight.js/styles/stackoverflow-light.css';
+import hljsCore from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import hljsVuePlugin from '@highlightjs/vue-plugin';
+hljsCore.registerLanguage('javascript', javascript);
 
 // highlightjs
 import hljs from 'highlight.js';
@@ -21,5 +28,6 @@ const app = createApp(App);
 app.use(router);
 // 引入v-md-editor预览组件
 app.use(VMdPreview);
+app.use(hljsVuePlugin);
 
 app.mount('#app');
