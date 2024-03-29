7515cb05c8e734f6c541fd265b36cbeec4d72ab9
Chen Yinying <chenyinying@patsnap.com>
2024-03-29 10:15:09 +0800

    add markdown

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
 
-app.use(router)
+// highlightjs
+import hljs from 'highlight.js';
+VMdPreview.use(githubTheme, {
+  Hljs: hljs
+});
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(router);
+// 引入v-md-editor预览组件
+app.use(VMdPreview);
+
+app.mount('#app');
--- src/views/ai/DemoView.vue
+++ src/views/ai/DemoView.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="demo">
+    <h1>DMP代码生成</h1>
+    <v-md-preview :text="text" height="400px"></v-md-preview>
+    <v-md-preview :text="text2" height="400px"></v-md-preview>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const text = ref(
+  '| 字段field                   | 字段名称         | 列表展示 | 检索展示 |\n' +
+    '|---------------------------|--------------|------|------|\n' +
+    '| translational_medicine_id | 转化医学ID       | Yes  | Yes  |\n' +
+    '| ct_result_id              | 临床结果ID       | Yes  | Yes  |\n' +
+    '| title                     | 标题           | Yes  | Yes  |\n' +
+    '| title_translation         | 标题翻译         | No   | No   |\n' +
+    '| source_code               | 来源code       | No   | Yes  |\n'
+);
+
+const text2 = ref('const a = 6');
+</script>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .demo {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    text-align: center;
   }
 }
 </style>
